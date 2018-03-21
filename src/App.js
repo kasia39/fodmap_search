2ffbf267f818a6f10172ff1531c71f20104af03a
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2018-03-21 20:47:27 +0100

    ii

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as data from './data'
 
 class App extends Component {
   render() {
+      console.log(data);
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to do nothing.
         </p>
       </div>
     );
