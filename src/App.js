af8b7ae316d9d9cf629d5c8b253d24e67c022aff
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2018-03-21 21:47:44 +0100

    prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {List, ListItem} from 'material-ui/List';
 import * as data from './data'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {TextField} from "material-ui";
+import Fuse from 'fuse.js'
+import {ActionDone, AlertWarning, ContentClear} from "material-ui/svg-icons/index";
 
+var options = {
+    keys: ['name'],
+};
+var fuse = new Fuse(data, options);
+var list = data.slice();
+
+function getIcon(iconName){
+    if(iconName === "yes"){
+        return <ActionDone/>
+    } else if (iconName === "conditionally") {
+        return <AlertWarning/>
+    } else if (iconName === "no") {
+        return <ContentClear />
+    }
+}
 class App extends Component {
-  render() {
-      console.log(data);
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to do nothing.
-        </p>
-      </div>
-    );
-  }
+    handleChange = (event) => {
+        if (event.target.value === "") {
+            list = data
+        } else {
+            list = fuse.search(event.target.value);
+        }
+        this.setState({
+            value: event.target.value,
+        });
+    };
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="App">
+                    <TextField
+                        floatingLabelText="Szukany produkt"
+                        onChange={this.handleChange}
+                    />
+                    <List/>
+                    {
+                        list.map((d, n) => <ListItem primaryText={d['name']} rightIcon={getIcon(d['statusw'])} key={n}/>)
+                    }
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
